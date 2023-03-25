c30f0c815101a4c212bb5654c73a21ee4677e9e2
keelangibb <keelangibb@gmail.com>
2023-03-25 01:23:50 -0500

    moved components folder, removed unused code

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,7 @@
 import { withClerkMiddleware } from "@clerk/nextjs/server";
-import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
-export default withClerkMiddleware((req: NextRequest) => {
+export default withClerkMiddleware(() => {
   console.log("Middleware running");
   return NextResponse.next();
 });
