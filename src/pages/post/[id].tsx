bf1bf8769b1564d2a84ca05a161e287fe10c61ba
keelangibb <keelangibb@gmail.com>
2023-03-31 14:06:00 -0500

    individual post view, ssg helper for post

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,16 +1,21 @@
 import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import { PageLayout } from "~/components/PageLayout";
+import PostView from "~/components/PostView";
 import { generateSSGHelper } from "~/server/helpers/ssgHelper";
+import { api } from "~/utils/api";
 
-const SinglePostPage: NextPage = () => {
+const SinglePostPage: NextPage<{ id: string }> = ({ id }) => {
+  const { data: post } = api.posts.getByPostId.useQuery({ id });
+  if (!post) return <div>404</div>;
+
   return (
     <>
       <Head>
         <title>Post</title>
       </Head>
       <PageLayout>
-        <div>Post View</div>
+        <PostView {...post} />
       </PageLayout>
     </>
   );
